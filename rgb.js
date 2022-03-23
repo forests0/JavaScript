d7d56eef6e0be91010025f2462eb6749020b56fb
남가연 <forestser00@gmail.com>
2022-03-23 10:36:24 +0900

    RGB 전환/JS

---
+++ rgb.js
@@ -0,0 +1,9 @@
+function red() {
+    document.getElementById('body').style.backgroundColor='red';
+}
+function blue() {
+    document.getElementById('body').style.backgroundColor='blue';
+}
+function green() {
+    document.getElementById('body').style.backgroundColor='green';
+}
