ecf5fe634c6efa6109196affbd73b9bfdbab5ef2
Forests <99336909+forests0@users.noreply.github.com>
2022-08-02 11:28:28 +0900

    react 헤더 연습

---
+++ JS_/header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Head() {
+  return (
+    <div>
+      <div className="head-div">
+        <img src="./logo512.png" width="40px" height="40px" />
+        <h3 className="head-h3">ReactFacts</h3>
+        <h3 className="head-righth3">React Course-Project1</h3>
+      </div>
+
+      <nav>
+        <h1 className="h1-title">Fun facts about React</h1>
+      </nav>
+    </div>
+  );
+}
